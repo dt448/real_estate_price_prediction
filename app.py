79b7ee77205a344ecaae60ef841ca2a173637027
DenThinkPad <Den@example.com>
2023-01-10 15:01:50 +0000

    simple api test

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ app=Flask(__name__)
 
 # load the model
 model = pickle.load(open('./model/model_v_0_1.pkl','rb'))
+scalar = pickle.load(open())
+
 
 @app.route('/')
 def home():
@@ -20,3 +22,11 @@ def predict_api():
 	data = request.json['data']
 	print(data)
 	print(np.array(list(data.values())[0]).reshape(1,-1))
+	new_data = scalar.transform(np.array(list(data.values())[0]).reshape(1,-1))
+	outout = model.predict(new_data)
+	print(output[0])
+	return jsonify(output)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
